744f68e5c11543359b589a7ceec06b41c8cda0f1
Mr Rogers <jon@bunnymatic.com>
2012-11-23 15:58:24 -0800

    * revert root to be blocks index

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,5 @@
 CustoBlocks::Application.routes.draw do
   resources :blocks
 
-  match 'sign_in'  => 'user_sessions#new',     as: :sign_in
-
-  match 'sign_out' => 'user_sessions#destroy', as: :sign_out
-
-  resources :user_sessions, only: [:new, :create, :destroy]
-
-  resources :registrations, only: [:new, :create, :activate]
-
-  match 'sign_up' => 'registrations#new', via: :get
-
-  match 'sign_up' => 'registrations#create', via: :post
-
-  match 'sign_up/:token/activate' => 'registrations#activate', via: :get
-
-  resources :password_resets, only: [:new, :create, :edit, :update]
-
-  resources :users
-
   root :to => 'blocks#index'
 end
